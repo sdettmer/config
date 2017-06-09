db4c19ff6776ef8b9d272d9f7e277e64b2b37109
Steffen Dettmer <steffen.dettmer@gmail.com>
2017-06-09 16:42:00 +0200

    cspatch.pl: auto-width lines, pipe mode.

--- bin/cspatch.pl
+++ bin/cspatch.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -np
 # This is config spec patcher hack.
 # This is an untested hack that may kill your pet.
 # It will use the output of "clearcase ci" to patch a config spec.
@@ -23,19 +23,29 @@ BEGIN
   $checkinlog = shift @ARGV || usage;
   $startlineregex = shift @ARGV || usage;
   {
-    my @loglines = ();
     open LOG, "$checkinlog" or die "Failed to open log $checkinlog: $!\n";
-    push @loglines, ""; # start with a blank line.
-    while (my $logline = <LOG>) {
+    my @loglines = (<LOG>);
+    close LOG, "$checkinlog" or die "Failed to close log $checkinlog: $!\n";
+    my $maxlen = -1;
+    # find length of longest log line. Be quick include everything, we just need an idea for tab size.
+    map { if (length > $maxlen) { $maxlen = length; } } @loglines;
+    $maxlen += -length('Checked in "" version "/main/1".') + 2;
+    $maxlen += -$maxlen % 4; # For Micha, who wants dividable by 4.
+
+    my @patchlines = ();
+    push @patchlines, ""; # start with a blank line.
+    foreach my $logline (@loglines) {
       chomp $logline;
       if ($logline =~ m/^Checked in "(\S*)" version "(\S*)"\.$/) {
          my ($element, $version) = ($1, $2);
          $element =~ s|/view/[^/]+/|/|; # cut optional "/view/sdettmer_latest/" prefix
          # Whitespace-free without quoting.
-         $logline = sprintf "%-184s %s", "    element $element", $version;
+         $logline = sprintf "    element %*s %s", -$maxlen, "$element", $version;
       } elsif ($logline =~ m/^Checked in (".*") version (".*")\.$/) {
+         my ($element, $version) = ($1, $2);
+         $element =~ s|/view/[^/]+/|/|; # cut optional "/view/sdettmer_latest/" prefix
          # whitespace in path -> quoting.
-         $logline = sprintf "%-184s %s", "    element $1", $2;
+         $logline = sprintf "    element %*s %s", -$maxlen+1, "$element", $version;
       } elsif ($logline =~ m/^cleartool: Warning: Version checked in is not selected by view\.$/) {
           # ignore
           $logline = undef;
@@ -43,10 +53,9 @@ BEGIN
          # comment
          $logline = "    # $logline";
       }
-      push @loglines, $logline unless (!defined($logline));
+      push @patchlines, $logline unless (!defined($logline));
     }
-    $patch = join("\n", @loglines);
-    close LOG, "$checkinlog" or die "Failed to close log $checkinlog: $!\n";
+    $patch = join("\n", @patchlines);
   }
   warn "log \"$checkinlog\", regex \"$startlineregex\", ARGV: @ARGV\n";
 }
