6018db64deca8010041ff17644ee646a94a5fdc0
Steffen Dettmer <steffen.dettmer@gmail.com>
2018-05-15 09:55:24 +0200

    multibuild: with .info file support

--- bin/multibuild.sh
+++ bin/multibuild.sh
@@ -15,10 +15,21 @@ while [ ${workingdir#*vobs} ] ; do
     #echo workingdir=$workingdir
     # Maybe we found it as sibling:
     [ -d "${workingdir}/vobs" ] && workingdir="${workingdir}/vobs"
+    # If we have multibuild.info (and no parameters), use its parameters
+    if [ -e "${workingdir}/multibuild.info" -a -z "$1" ] ; then
+        echo "Using ${workingdir}/multibuild.info"
+        source "${workingdir}/multibuild.info"
+        # multibuild="$topsrc/vobs/tisc_ccu-c/load/bld/multibuild.sh"
+        multibuild="$cmakesrcdir/multibuild.sh"
+        exec $multibuild -n "$name" -p "$project" -t "$platformname" -b "$build_type"
+        echo "Failed to execute multibuild.sh (via multibuild.info)" >&2
+        exit 2
+    fi
 done
 multibuild="$workingdir/tisc_ccu-c/load/bld/multibuild.sh"
 #echo multibuild=$multibuild
 
 # exec does the error handling here (gives e.g. "No such file or directory")
 exec $multibuild "$@"
-
+echo "Failed to execute multibuild" >&2
+exit 2
